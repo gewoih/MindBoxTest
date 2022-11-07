abf4452e5a14070e1f8babc0174e6b987c8f9317
Никита Раненко <nranenko@bk.ru>
2022-11-08 00:39:09 +0700

    Тесты для DoubleExtenstions

---
+++ MindBoxLibTests/DoubleExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using MindBoxLib.Extensions;
+
+namespace MindBoxLibTests
+{
+    public class DoubleExtensionsTests
+    {
+        [TestCase(1, 1, -1)]
+        public void IsEqualsWithPrecision_PrecisionLessThanOne_ThrowsArgumentException(double leftValue, double rightValue, int precision)
+        {
+            TestDelegate actual = () =>
+            {
+                leftValue.IsEqualsWithPrecision(rightValue, precision);
+            };
+
+            Assert.Throws<ArgumentException>(actual);
+        }
+
+        [TestCase(1.4, 1.2, 0, true)]
+        [TestCase(1.5431, 1.543412, 3, true)]
+        [TestCase(Math.PI, 3.145415, 6, false)]
+        [TestCase(1.13, 1.14, 2, false)]
+        public void IsEqualsWithPrecision_FinitePositiveValues_ExpectedResult(double leftValue, double rightValue, int precision, bool expected)
+        {
+            bool actual = leftValue.IsEqualsWithPrecision(rightValue, precision);
+
+            Assert.That(Is.Equals(actual, expected), Is.True);
+        }
+    }
+}
